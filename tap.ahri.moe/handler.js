b7552680d32e5544df3289c61a395b9a110bc81f
RiceGnat <ricegnat@gmail.com>
2017-09-06 22:26:38 -0500

    added deck info

--- tap.ahri.moe/handler.js
+++ tap.ahri.moe/handler.js
@@ -1,19 +1,79 @@
 const http = require("http");
 const url = require("url");
-const parse = require('csv-parse/lib/sync');
+const parse = require("csv-parse/lib/sync");
+const $ = require("cheerio");
 
 const host = "http://tappedout.net/mtg-decks/";
+const printFlag = "fmt=printable";
 const csvFlag = "fmt=csv";
 
-var deck;
+var deck = {
+    name: "",
+    description: "",
+    url: "",
+    author: "",
+    format: "",
+    list : []
+};
 
-function ExtractTitle(htmlString) {
-    return $(".well-jumbotron h2", htmlString).text().trim();
+function GetInfo(target, res) {
+    var path = host + target + "/?" + printFlag;
+
+    var deckRequest = http.get(path, (deckResponse) => {
+        if (deckResponse.statusCode == 200) {
+            var responseString = "";
+            deckResponse.on("data", (chunk) => {
+                responseString += chunk;
+            }).on("end", () => {
+                var name = $("h2", responseString).first().text();
+                deck.name = name.substr(1, name.length - 2);
+                deck.description = $("p", responseString).first().text();
+                deck.author = $("tr:contains(User) > td", responseString).last().text();
+                deck.format = $("tr:contains(Format) > td", responseString).last().text();
+
+                GetDeck(target, res);
+            });
+        }
+        else {
+            throw `Info request returned status code ${deckResponse.statusCode}`;
+        }
+    });
+    deckRequest.end();
+}
+
+function GetDeck(target, res) {
+    var path = host + target + "/?" + csvFlag;
+
+    var deckRequest = http.get(path, (deckResponse) => {
+        if (deckResponse.statusCode == 200) {
+            var responseString = "";
+            deckResponse.on("data", (chunk) => {
+                responseString += chunk;
+            }).on("end", () => {
+                // This is a typo from tappedout.net
+                responseString = responseString.replace("Languange", "Language");
+
+                deck.list = parse(responseString, { columns: true });
+
+                ReturnDeck(res);
+            });
+        }
+        else {
+            throw `Deck request returned status code ${deckResponse.statusCode}`;
+        }
+    });
+    deckRequest.end();
+}
+
+function ReturnDeck(res) {
+    res.writeHead(200, { "Content-Type": "text/json" });
+    res.end(JSON.stringify(deck));
 }
 
 function LoadDeck(req, res) {
     var targetName = url.parse(req.url).pathname.substr(1).replace(/\/$/, "");
-    var path = host + targetName + "/?" + csvFlag;
+
+    deck.url = host + targetName + "/";
 
     try {
         if (targetName == "favicon.ico") {
@@ -23,29 +83,10 @@ function LoadDeck(req, res) {
         }
         else if (targetName == "") {
             res.writeHead(200);
-            res.end(); 
+            res.end();
         }
         else {
-            var deckRequest = http.request(path, (deckResponse) => {
-                if (deckResponse.statusCode == 200) {
-                    var responseString = "";
-                    deckResponse.on("data", (chunk) => {
-                        responseString += chunk;
-                    }).on("end", () => {
-                        // This is a typo from tappedout.net
-                        responseString = responseString.replace("Languange", "Language");
-
-                        deck = parse(responseString, { columns: true });
-
-                        res.writeHead(200, { "Content-Type": "text/json" });
-                        res.end(JSON.stringify(deck));
-                    });
-                }
-                else {
-                    throw `Deck request returned status code ${deckResponse.statusCode}`;
-                }
-            });
-            deckRequest.end();
+            GetInfo(targetName, res);
         }
     }
     catch (ex) {
