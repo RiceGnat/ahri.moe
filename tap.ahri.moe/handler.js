ea40583c9a937303cb7fd6626060d1de79d021e9
RiceGnat <ricegnat@gmail.com>
2018-04-07 18:33:41 -0500

    testing

--- tap.ahri.moe/handler.js
+++ tap.ahri.moe/handler.js
@@ -51,7 +51,7 @@ function GetInfo(deck, target, res) {
             });
         }
         else {
-            ReturnError(res, `Info request returned status code ${deckResponse.statusCode}\n${JSON.stringify(deckResponse.headers)}\n${JSON.stringify(deckResponse.req)}`);
+            ReturnError(res, `Info request returned status code ${deckResponse.statusCode}\n${JSON.stringify(deckResponse.headers)}`);
         }
         });
     deckRequest.end();
