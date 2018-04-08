15299e10d423928f6d1d21ac9ec0568bb4d91ff8
RiceGnat <ricegnat@gmail.com>
2018-04-07 19:03:29 -0500

    testing

--- tap.ahri.moe/handler.js
+++ tap.ahri.moe/handler.js
@@ -13,6 +13,8 @@ const hostUserPath = "/users/"
 const printFlag = "fmt=printable";
 const csvFlag = "fmt=csv";
 const requestHeaders = {
+    "Cache-Control": "no-cache",
+    "Connection": "keep-alive",
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:59.0) Gecko/20100101 Firefox/59.0"
 };
 
