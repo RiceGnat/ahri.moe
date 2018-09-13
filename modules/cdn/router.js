0e0771b65690a07f231b6511e344a07b69d9a4ff
RiceGnat <ricegnat@gmail.com>
2018-09-13 18:28:50 -0500

    gandi doesn't support submodules

--- modules/cdn/router.js
+++ modules/cdn/router.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const path = require("path");
-console.log(path.join(__dirname, "web-resources/public"));
+
 module.exports = express.Router()
-.use("/", express.static(path.join(__dirname, "web-resources/public")));
+.use("/", express.static("node_modules/web-resources/public"));
