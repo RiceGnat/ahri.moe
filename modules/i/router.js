819f2c2e5e6471686169f0c849f4ed5b5053f38e
RiceGnat <ricegnat@gmail.com>
2018-09-13 19:23:51 -0500

    changed meta flag to query instead of path

--- modules/i/router.js
+++ modules/i/router.js
@@ -1,14 +1,6 @@
 const express = require("express");
 const request = require("request");
 
-const defaultExtension = "jpg";
-
-const getSourceUrl = targetName => {
-    const target = targetName.split(".");
-    const extension = target[1] || defaultExtension;
-    return `http://www.ricegnat.com/junk/${target[0]}.${extension}`;
-};
-
 module.exports = express.Router()
 .get("/", (req, res) => {
     return res.status(204).send();
@@ -18,48 +10,45 @@ module.exports = express.Router()
 })
 .get("/:targetName", (req, res) => {
     const targetName = req.params.targetName;
+    const useMeta = req.query.t !== undefined;
 
-    request.get({
-        url: getSourceUrl(targetName),
+    const target = targetName.split(".");
+    const extension = target[1] || "jpg";
+    const url = `http://www.ricegnat.com/junk/${target[0]}.${extension}`;
+
+    // Either get image or check that image exists
+    const options = !useMeta ? {
+        url: url,
+        method: "GET",
         encoding: null
-    }, (err, resp, body) => {
-        if (err)
-            return res.status(500).send();
-
-        if (resp.statusCode !== 200)
-            return res.status(resp.statusCode).send(`Remote server returned with ${resp.statusCode}: ${resp.statusMessage}`);
-        
-        res.set("Content-Type", resp.headers["content-type"]).send(body);
-    })
-})
-.get("/:targetName/t", (req, res) => {
-    const targetName = req.params.targetName;
-    const twitterHandle = "@ricegnat";
-    const title = "Screenshot";
-
-    const url = getSourceUrl(targetName);
-
-    // Make a HEAD request to check that the image is present
-    request({
+    } : {
         url: url,
         method: "HEAD"
-    }, (err, resp) => {
+    };
+
+    request(options, (err, resp, data) => {
         if (err)
             return res.status(500).send();
 
         if (resp.statusCode !== 200)
             return res.status(resp.statusCode).send(`Remote server returned with ${resp.statusCode}: ${resp.statusMessage}`);
         
-        // Construct meta tag response
-        const redirectUrl = `//${req.headers.host}/${targetName}`;
-        const timestamp = resp.headers["last-modified"];
-        const body  = `<meta name="twitter:card" content="summary_large_image" />`
-                    + `<meta name="twitter:site" content="${twitterHandle}" />`
-                    + `<meta name="twitter:title" content="${title}" />`
-                    + `<meta name="twitter:description" content="${timestamp}" />`
-                    + `<meta name="twitter:image" content="${url}" />`
-                    + `<script>window.location.replace("${redirectUrl}");</script>`;
+        if (!useMeta) {
+            // Send image
+            res.set("Content-Type", resp.headers["content-type"]).send(data);
+        }
+        else {
+            // Construct meta tag response
+            const redirectUrl = `//${req.headers.host}/${targetName}`;
+            const timestamp = resp.headers["last-modified"];
+            const body  = `<meta name="twitter:card" content="summary_large_image" />`
+                        + `<meta name="twitter:site" content="@ricegnat" />`
+                        + `<meta name="twitter:title" content="Screenshot" />`
+                        + `<meta name="twitter:description" content="${timestamp}" />`
+                        + `<meta name="twitter:image" content="${url}" />`
+                        + `<script>window.location.replace("${redirectUrl}");</script>`;
 
-        res.send(body);
+            res.send(body);
+        }
     })
 });
